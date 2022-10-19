a918aa251b86c79ca2a06c3dba8221c3585eea10
i438382 <v.wnuck@student.fontys.nl>
2022-10-19 14:21:28 +0200

    test push

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Footer from './src/components/footer/footer';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Footer/>
       <StatusBar style="auto" />
     </View>
   );
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Footer = () => {
-
+    return (
+        <p>Test 123</p>
+    )
 }
 
 export default Footer;
