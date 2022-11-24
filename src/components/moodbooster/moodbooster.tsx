768c33a7a51ecc4dc62785ee77ba1fc9b5b2f2ab
Stan Wilmsen <413970@student.fontys.nl>
2022-11-24 10:17:44 +0100

    Changed cardtext

--- src/components/moodbooster/moodbooster.tsx
+++ src/components/moodbooster/moodbooster.tsx
@@ -10,30 +10,36 @@ import {
   Title,
   Paragraph,
 } from "react-native-paper";
-import { useFonts, Poppins_600SemiBold, Poppins_400Regular} from '@expo-google-fonts/poppins';
+import {
+  useFonts,
+  Poppins_600SemiBold,
+  Poppins_400Regular,
+} from "@expo-google-fonts/poppins";
 
 const Moodbooster = (props) => {
   const [todos, setTodos] = useState([
     {
       text: "Code a website!",
-      complete: true,
+      complete: "Start",
       points: 1,
     },
     {
       text: "Make videos!",
-      complete: false,
+      complete: "Start",
       points: 2,
     },
     {
       text: "Make a todo list!",
-      complete: false,
+      complete: "Start",
       points: 3,
     },
   ]);
 
+  const [cardState, setCardState] = useState();
+
   let [fontsLoaded] = useFonts({
     Poppins_600SemiBold,
-    Poppins_400Regular
+    Poppins_400Regular,
   });
 
   if (!fontsLoaded) {
@@ -41,40 +47,52 @@ const Moodbooster = (props) => {
   }
 
   function handleTodoClick(index) {
+    //send stuff to back end and update front end.
     let itemsCopy = [...todos];
-    props.onComplete(itemsCopy[index].points);
-    itemsCopy.splice(index, 1);
+    // props.onComplete(itemsCopy[index].points);
+    // itemsCopy.splice(index, 1);
+    itemsCopy[index].complete = "Complete";
+    // console.log(todos[index].complete)
     setTodos(itemsCopy);
   }
 
   return (
     <View>
-      <Text style={{ fontFamily: 'Poppins_600SemiBold', fontSize: 20, margin: 10 }}>Moodboosters</Text>
+      <Text
+        style={{ fontFamily: "Poppins_600SemiBold", fontSize: 20, margin: 10 }}
+      >
+        Moodboosters
+      </Text>
       <View>
         {todos.map((item, index) => (
-          <Surface style={styles.surface} elevation={1} key={index} >
+          <Card style={styles.card} mode="outlined" key={index}>
             <Card.Title
-              title={item.text} titleStyle={{ fontFamily: 'Poppins_400Regular' }}
-              // left={(props) => <Avatar.Icon {...props} icon="folder" />}
+              title={item.text}
+              titleStyle={{ fontFamily: "Poppins_400Regular" }}
               right={(props) => (
                 <View style={styles.buttons}>
                   <IconButton
                     {...props}
+                    mode="outlined"
                     icon="account-plus"
                     onPress={() => {}}
                   />
                   <Button
                     mode="contained"
                     buttonColor="#419FD9"
-                    labelStyle={{ fontFamily: 'Poppins_600SemiBold' }}
+                    labelStyle={{
+                      fontFamily: "Poppins_600SemiBold",
+                      textTransform: "uppercase",
+                    }}
                     onPress={() => handleTodoClick(index)}
                   >
-                    Done
+                    {todos[index].complete}
                   </Button>
                 </View>
               )}
             />
-          </Surface>
+          </Card>
+          
         ))}
       </View>
     </View>
@@ -86,14 +104,13 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     alignItems: "center",
-
-  },
-  surface: {
-    borderRadius: 5,
     paddingRight: 10,
+  },
+  card: {
+    borderRadius: 5,
     marginHorizontal: 10,
     marginVertical: 6,
-    fontFamily: 'Poppins_600SemiBold'
+    fontFamily: "Poppins_600SemiBold",
   },
 });
 
