42045271e07390b018ccf4829aa92150cc3b4df0
shonie2xx <aleksandarlekov11@gmail.com>
2022-12-14 01:08:22 +0100

    works like a charm

--- src/screens/page-friends/page-friends.tsx
+++ src/screens/page-friends/page-friends.tsx
@@ -10,49 +10,61 @@ import {
 import { useFonts, Poppins_600SemiBold, Poppins_400Regular } from '@expo-google-fonts/poppins';
 import { useEffect, useState, useContext } from "react";
 import { getFriends } from "../../services/friendsService";
-import { getAllUsers} from "../../services/userService";
+import { getAllUsers } from "../../services/userService";
 import { AuthContext } from "../../context/AuthContext";
+import { __handlePersistedRegistrationInfoAsync } from "expo-notifications/build/DevicePushTokenAutoRegistration.fx";
 
 
 const PageFriends = () => {
   const wave = require("../../../assets/wave.png");
 
-  const {accessToken} = useContext(AuthContext);
+  const { accessToken } = useContext(AuthContext);
 
   const [users, setUsers] = useState([]);
   const [friends, setFriends] = useState([])
-  const [nonFriends, setNonFriends] = useState([]);
+  const [notFriends, setNotFriends] = useState([]);
+  const [isFriends, setIsFriends] = useState(false);
+
   useEffect(() => {
-    //fetchFriends()
-    handleFriends()
+    handleData();
     console.log(friends)
   }, [])
 
+  const fetchUsers = async () => {
+    try {
+      const res = await getAllUsers(accessToken);
+      setUsers(res);
+    } catch (err) {
+      console.log(err)
+    }
+  }
   const fetchFriends = async () => {
-    const res = await getFriends(accessToken);
-    console.log("friends", res)
-    //const json = await res.json();
-    setFriends(res);
-  };
-  
-  const getUsers = async () => {
-    const res = await getAllUsers(accessToken);
-    // console.log("users", res)
-    setUsers(res);
+    try {
+      const res = await getFriends(accessToken);
+      setFriends(res);
+    } catch (err) {
+      console.log(err)
+    }
   }
 
-  const handleFriends = async () => {
+  const handleData = async () => {
+    await fetchUsers();
     await fetchFriends();
-    await getUsers();
-     for (var i = 0 ; i < users.length; i ++) {
-      // if friends doesn't have a user, then add it to the nonFriends array
-      if(!friends.includes(users[i])) {
-        
-        setNonFriends(nonFriends => [...nonFriends, users[i]]);
-      }
-     }
-     console.log("Non friends", nonFriends);
+
+    if (users.length > 0) {
+        const notFriends = users.filter(user => !friends.includes(user.id))
+        console.log("not friends", notFriends)
+        setNotFriends(notFriends);
+    }
+     
+    if (friends.length === 0) {
+      setIsFriends(false);
+    } else {
+      setIsFriends(true);
+    }
+    console.log("friends", friends)
   }
+
 
   let [fontsLoaded] = useFonts({
     Poppins_600SemiBold,
@@ -63,59 +75,35 @@ const PageFriends = () => {
     return null;
   }
   return (
-    // <ScrollView style={styles.screen}>
-    //   <ImageBackground source={wave} style={styles.wave}>
-    //     <View>
-    //       <Text>Friends</Text>
-    //       {friends.map((item, index) => {
-    //         <Card style={styles.surface} elevation={1} key={index}>
-    //         <Card.Title title={item.name} />
-    //         <Card.Actions>
-    //            <Button mode="contained" onPress={() => console.log('Pressed')}>REMOVE</Button> 
-    //         </Card.Actions>
-    //       </Card>
-    //       })}
-    //       </View>
-    //     </ImageBackground>
-    //   <View>
-    //     <Text>All other users</Text>
-    //     {nonFriends.map((item, index) => (
-          // <Card style={styles.surface} elevation={1} key={index}>
-          //   <Card.Title title={item.name} />
-          //   <Card.Actions>
-          //      <Button mode="contained" onPress={() => console.log('Pressed')}>REMOVE</Button> 
-          //   </Card.Actions>
-          // </Card>
-    //     ))}
-    //   </View>
-      
-    //   </ScrollView>
     <ScrollView style={styles.screen}>
       <ImageBackground source={wave} style={styles.wave}>
-      <View>
-        
-          <Text>Friends</Text>
-        {friends.map( (item, index) => (
-          <Card style={styles.surface} elevation={1} key={index}>
-          <Card.Title title={item.name} />
-          <Card.Actions>
-             <Button mode="contained" onPress={() => console.log('Pressed')}>REMOVE</Button> 
-          </Card.Actions>
-        </Card>
-        ))}
-        
-      </View>
-      <View>
-        <Text>Other people</Text>
-        {nonFriends.map((item, index) => (
-          <Card style={styles.surface} elevation={1} key={index}>
-          <Card.Title title={item.name} />
-          <Card.Actions>
-             <Button mode="contained" onPress={() => console.log('Pressed')}>ADD</Button> 
-          </Card.Actions>
-        </Card>
-        ))}
-      </View>
+        <View>
+
+          <Text style={styles.title}>Friends</Text>
+
+
+          {isFriends ? friends.map((item, index) => (
+            <Card style={styles.surface} elevation={1} key={index}>
+              <Card.Title title={item.name} />
+              <Card.Actions>
+                <Button mode="contained" onPress={() => console.log('Pressed')}>REMOVE</Button>
+              </Card.Actions>
+            </Card>
+          )) : <Text>No friends yet! Make some friends by sending a friend request!</Text>}
+
+
+        </View>
+        <View>
+          <Text style={styles.title}>Other people</Text>
+          {notFriends ? notFriends.map((item, index) => (
+            <Card style={styles.surface} elevation={1} key={index}>
+              <Card.Title title={item.name} />
+              <Card.Actions>
+                <Button mode="contained" onPress={() => console.log('Pressed')}>ADD</Button>
+              </Card.Actions>
+            </Card>
+          )) : <Text>No users</Text>}
+        </View>
       </ImageBackground>
     </ScrollView>
   );
@@ -149,10 +137,10 @@ const styles = StyleSheet.create({
     resizeMode: "center"
   },
   title: {
-    fontFamily: 'Poppins_600SemiBold', 
-    fontSize: 20, 
-    margin: 8, 
-    color: '#031D29', 
+    fontFamily: 'Poppins_600SemiBold',
+    fontSize: 20,
+    margin: 8,
+    color: '#031D29',
     paddingLeft: 16
   }
 });
