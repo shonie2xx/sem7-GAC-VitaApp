55cd0a301d83e7faf30d28463cad0b131b9123fb
Rosheuvel,Yannick Y.W.E <y.rosheuvel@student.fontys.nl>
2023-01-09 14:43:23 +0100

    .

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 import { protectedResources } from "../../authConfig";
 
+
+
 const url = protectedResources.apiUser.endpoint;
 
 export async function getAllUsers(token) {
+  var response = await axios.get(url + "all", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+}
+
+export async function setUserExpoPushToken(token) {
   var response = await axios.get(url + "all", {
     headers: { Authorization: `Bearer ${token}` },
   });
@@ -20,6 +29,38 @@ export async function getUser(token) {
 export async function checkUser(token) {
   
   var response = await axios.get(url + "login/check", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+}
+
+export async function SetModalVisable(isModalVisable) {
+  
+  var response = await axios.post(url + "modalvisible/" + {isModalVisable}, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+}
+
+export async function GetModalVisable(token) {
+  
+  var response = await axios.get(url + "modalvisible", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+}
+
+export async function GetDate(token) {
+  
+  var response = await axios.get(url + "date", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data;
+}
+
+export async function SetDate(date) {
+  
+  var response = await axios.post(url + "modalvisible/" + {date}, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
