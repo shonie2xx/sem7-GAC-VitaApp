abb64c9e83f693d8dd53eb449a4d612b4c10368d
Stan Wilmsen <413970@student.fontys.nl>
2023-01-09 21:13:51 +0100

    Completed manual moodchange

--- src/screens/page-home/page-home.tsx
+++ src/screens/page-home/page-home.tsx
@@ -1,4 +1,13 @@
-import { View, StyleSheet, Image, ImageBackground } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  TouchableHighlight,
+  Touchable,
+  TouchableWithoutFeedback,
+} from "react-native";
 import React, { useState, useEffect, useContext } from "react";
 import { Text, Card, Avatar, IconButton } from "react-native-paper";
 import Moodbooster from "../../components/moodbooster/moodbooster";
@@ -6,6 +15,7 @@ import Modal from "react-native-modal";
 import Moodperson from "../../../assets/moodperson.svg";
 import Moodperson_sad from "../../../assets/moodperson_sad.svg";
 import Moodperson_neutral from "../../../assets/moodperson_neutral.svg";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 import StartupMood from "../../components/PopUps/StartupMood";
 import {
@@ -20,30 +30,59 @@ import {
   Poppins_700Bold,
   Poppins_600SemiBold,
 } from "@expo-google-fonts/poppins";
-import { getUser } from "../../services/userService";
+import { getUser, updateUserMood } from "../../services/userService";
 import { AuthContext } from "../../context/AuthContext";
 import ChallengeFriends from "../../components/challengeFriends/challengeFriends";
 import { MoodboosterContext } from "./moodboosterContext";
 import * as SecureStore from "expo-secure-store";
+import TertiaryBtn from "../../components/buttons/TertiaryBtn";
 
 const PageHome = ({ navigation }) => {
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
   const [moodboosterRequests, setMoodboosterRequests] = useState(0);
   const [picsource, setPicsource] = useState(
     require("../../../assets/moodperson.svg")
   );
+  const [isModalVisible, setModalVisible] = useState(false);
   const wave = require("../../../assets/wave.png");
 
   const [mood, setMood] = useState(10);
-
+  const [changeMood, setChangeMood] = useState(0);
   const userMood = async () => {
     var userData = await getUser(accessToken);
     const currentUser = JSON.parse(await SecureStore.getItemAsync("User"));
-    setName(currentUser.name)
-    // console.log(userData);
+    setName(currentUser.name);
     setMood(userData.mood);
   };
-
+  const toggleModalOn = () => {
+    setChangeMood(mood);
+    setModalVisible(!isModalVisible);
+  };
+  const toggleModalOff = async () => {
+    if (changeMood !== mood) {
+      await updateUserMood(accessToken, changeMood);
+      userMood();
+      setModalVisible(!isModalVisible);
+      console.log("Updated Mood!");
+    } else {
+      setModalVisible(!isModalVisible);
+      console.log("Cancelled!");
+    }
+  };
+  const moodDown = () => {
+    if (changeMood <= 0) {
+      setChangeMood(0);
+    } else {
+      setChangeMood(changeMood - 1);
+    }
+  };
+  const moodUp = () => {
+    if (changeMood >= 50) {
+      setChangeMood(50);
+    } else {
+      setChangeMood(changeMood + 1);
+    }
+  };
   const { accessToken } = useContext(AuthContext);
 
   useEffect(() => {
@@ -51,12 +90,12 @@ const PageHome = ({ navigation }) => {
   }, []);
 
   const ChangePic = () => {
-    const userMoodConditions = mood
+    const userMoodConditions = mood;
     if (userMoodConditions > 7) {
       return <Moodperson />;
     } else if (userMoodConditions <= 7 && userMoodConditions >= 4) {
       return <Moodperson_neutral />;
-    } else if (userMoodConditions < 4)  {
+    } else if (userMoodConditions < 4) {
       return <Moodperson_sad />;
     }
   };
@@ -72,30 +111,62 @@ const PageHome = ({ navigation }) => {
   }
 
   return (
-    <MoodboosterContext.Provider value={ {moodboosterRequests, setMoodboosterRequests} }>
-    <View style={styles.screen}>
-      <StartupMood changeMood={userMood} />
-      <View style={styles.top}>
-        <ImageBackground source={wave} style={styles.wave}>
-          <View style={styles.homeTop}>
-            <Text style={styles.heading2}>{name}</Text>
-            <ChangePic/>
-            <View style={styles.moodcontainer}>
-              <Image
-                style={styles.moodbg}
-                source={require("../../../assets/moodbg2.png")}
-              />
-              <Text style={styles.moodnmbr}>{mood}</Text>
+    <MoodboosterContext.Provider
+      value={{ moodboosterRequests, setMoodboosterRequests }}
+    >
+      <View style={styles.screen}>
+        <StartupMood changeMood={userMood} />
+        <View style={styles.top}>
+          <ImageBackground source={wave} style={styles.wave}>
+            <View style={styles.homeTop}>
+              <Text style={styles.heading2}>{name}</Text>
+              <ChangePic />
+              <View style={styles.moodcontainer}>
+                <TouchableOpacity onPress={toggleModalOn}>
+                  <Image
+                    style={styles.moodbg}
+                    source={require("../../../assets/moodbg2.png")}
+                  />
+                </TouchableOpacity>
+                <Text style={styles.moodnmbr}>{mood}</Text>
+
+                <Modal isVisible={isModalVisible} style={styles.modal}>
+                  <View style={styles.changeMoodModal}>
+                    <Text style={styles.changeMoodTitle}>
+                      Manually change mood
+                    </Text>
+                    <View style={styles.moodModalGroup}>
+                      <TouchableOpacity onPress={moodDown}>
+                        <Ionicons
+                          style={styles.modalIcons}
+                          name="remove-circle-outline"
+                          size={40}
+                          color="#052D40"
+                        />
+                      </TouchableOpacity>
+                      <Text style={styles.moodnmbrModal}>{changeMood}</Text>
+                      <TouchableOpacity onPress={moodUp}>
+                        <Ionicons
+                          style={styles.modalIcons}
+                          name="add-circle-outline"
+                          size={40}
+                          color="#052D40"
+                        />
+                      </TouchableOpacity>
+                    </View>
+                    <TertiaryBtn text="DONE" onPress={toggleModalOff} />
+                  </View>
+                </Modal>
+              </View>
             </View>
-          </View>
-        </ImageBackground>
+          </ImageBackground>
+        </View>
+        <View style={styles.moodboostertop}>
+          <Text style={styles.moodtitle}>Today's moodboosters</Text>
+          <ChallengeFriends />
+        </View>
+        <Moodbooster changeMood={userMood} />
       </View>
-      <View style={styles.moodboostertop}>
-        <Text style={styles.moodtitle}>Today's moodboosters</Text>
-        <ChallengeFriends />
-      </View>
-      <Moodbooster changeMood={userMood} />
-    </View>
     </MoodboosterContext.Provider>
   );
 };
@@ -131,12 +202,19 @@ const styles = StyleSheet.create({
   },
   moodnmbr: {
     position: "absolute",
-    fontSize: 34,
-    fontWeight: "bold",
+    fontSize: 32,
+    fontFamily: "Poppins_600SemiBold",
     color: "#FFFFFF",
     zIndex: 3,
     textAlign: "center",
     marginBottom: 20,
+  },
+  moodnmbrModal: {
+    fontSize: 32,
+    fontFamily: "Poppins_600SemiBold",
+    zIndex: 3,
+    textAlign: "center",
+    color: "#052D40",
   },
   moodbg: {
     zIndex: 2,
@@ -165,6 +243,37 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     marginTop: 16,
+  },
+  changeMoodTitle: {
+    fontFamily: "Poppins_600SemiBold",
+    fontSize: 24,
+    color: "#052D40",
+  },
+  changeMoodModal: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 18,
+    width: "100%",
+    height: "30%",
+    borderRadius: 8,
+    backgroundColor: "white",
+  },
+  modal: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  icon: {
+    paddingHorizontal: 8,
+  },
+  moodModalGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: "50%",
+    // backgroundColor: "black",
+  },
+  modalIcons: {
+    paddingLeft: 4,
   },
 });
 
