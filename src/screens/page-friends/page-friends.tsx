a792e6a947b37fd062a79ad063d29ae0f8575192
shonie2xx <aleksandarlekov11@gmail.com>
2023-01-10 14:57:17 +0100

    friends fixed

--- src/screens/page-friends/page-friends.tsx
+++ src/screens/page-friends/page-friends.tsx
@@ -1,107 +1,135 @@
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, ImageBackground, Pressable, SafeAreaView, RefreshControl, Image } from "react-native";
-import { useFonts, Poppins_600SemiBold, Poppins_400Regular } from '@expo-google-fonts/poppins';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  ImageBackground,
+  Pressable,
+  SafeAreaView,
+  RefreshControl,
+  Image,
+} from "react-native";
+import {
+  useFonts,
+  Poppins_600SemiBold,
+  Poppins_400Regular,
+} from "@expo-google-fonts/poppins";
 import { useEffect, useState, useContext, useRef, useCallback } from "react";
-import { addFriend, getFriends, getSendedRequests, removeFriend, cancelFrRequest } from "../../services/friendsService";
+import {
+  addFriend,
+  getFriends,
+  getSendedRequests,
+  removeFriend,
+  cancelFrRequest,
+} from "../../services/friendsService";
 import { getAllUsers } from "../../services/userService";
 import { AuthContext } from "../../context/AuthContext";
 import { __handlePersistedRegistrationInfoAsync } from "expo-notifications/build/DevicePushTokenAutoRegistration.fx";
 import SecondaryBtn from "../../components/buttons/SecondaryBtn";
 import PrimaryBtn from "../../components/buttons/PrimaryBtn";
 import { Item } from "react-native-paper/lib/typescript/components/Drawer/Drawer";
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from "expo-secure-store";
 import ProfilePic from "../../../assets/pfp.svg";
 import Bg from "../../../assets/wave.svg";
 
 const wait = (timeout) => {
-  return new Promise(resolve => setTimeout(resolve, timeout));
-}
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+};
 
 const PageFriends = () => {
   const wave = require("../../../assets/wave.png");
 
   const { accessToken } = useContext(AuthContext);
 
-
   const [refreshing, setRefreshing] = useState(false);
 
   const [users, setUsers] = useState([]);
-  const [friends, setFriends] = useState([])
+  const [friends, setFriends] = useState([]);
   const [otherPeople, setOtherPeople] = useState([]);
   const [invites, setInvites] = useState([]);
 
   useEffect(() => {
-    datafetching()
-  }, [])
+    datafetching();
+  }, []);
 
+  useEffect(() => {
+    filterarrays();
+  }, [invites]);
 
-  
-  const datafetching = useCallback( async () => {
-    const currentUser =  JSON.parse(await SecureStore.getItemAsync("User"));
+  const datafetching = useCallback(async () => {
+    getAllUsers(accessToken)
+      .then((users) => {
+        //console.log("users", users);
+        setUsers(users);
+      })
+      .then(() => {
+        getFriends(accessToken)
+          .then((friends) => {
+            //console.log("friends", friends);
+            setFriends(friends);
+          })
+          .then(() => {
+            getSendedRequests(accessToken).then((invites) => {
+              //console.log("invites", invites);
+              setInvites(invites);
+            }).then( () => {
+              filterarrays();
+            });
+          });
+      });
+  }, []);
 
-    const arrayusers = await getAllUsers(accessToken);
-    setUsers(arrayusers);
-    const arrayfriends = await getFriends(accessToken);
-    setFriends(arrayfriends);
-    const arrayinvites = await getSendedRequests(accessToken);
-    setInvites(arrayinvites);
+  const filterarrays = async () => {
+    const currentUser = JSON.parse(await SecureStore.getItemAsync("User"));
+    if (users.length > 0) {
+      const otherPeps = users.filter(
+        (user) =>
+          !JSON.stringify(friends).includes(user.id) &&
+          !JSON.stringify(invites).includes(user.id) &&
+          user.id !== currentUser.id
+      );
+      setOtherPeople(otherPeps);
+    }
+  };
 
-    const abs = JSON.stringify(arrayfriends);
-    const fds = JSON.stringify(arrayinvites);
-    const withoutFriends = users.filter(user => !abs.includes(user.id) && user.id !== currentUser.id);
-    const otherPeops = withoutFriends.filter(user => !fds.includes(user.id));
-    
-    console.log("otherPeops: " + JSON.stringify(otherPeops));
-    setOtherPeople(otherPeops);
-    
-    setRefreshing(true);
-    wait(2000).then(() => setRefreshing(false));
-  }, [] )
-
-  const handleAddFriends = async (id: any) => {
+  const sendInvite = async (id: any) => {
     try {
       const res = await addFriend(accessToken, id);
-      console.log("response add friend", res.status)
       if (res.ok) {
-        //alert
-
-        //setOtherPeople(otherPeople.filter(user => user.id !== id)); // filter friends from users
+        datafetching();
       }
-      // console.log(res.status)
     } catch (err) {
-      console.log("Adding friend failed", err)
+      console.log("Adding friend failed", err);
     }
-  }
+  };
 
-  const handleRemoveFriend = async (id) => {
+  const deleteFriend = async (id) => {
     try {
       const res = await removeFriend(accessToken, id);
       if (res.status === 200) {
-        //alert
-
-        //filter friends array
-        //setFriends(friends.filter(item => item.id !== id))
+        datafetching();
       }
     } catch (err) {
       console.log("can't remove friend", err);
     }
-  }
+  };
 
-  const handleCancelRequest = async (id) => {
+  const cancelInvite = async (id) => {
     try {
+      console.log("ID OF CANCELATION", id)
       const res = await cancelFrRequest(accessToken, id);
-      if(res.status === 200) {
-        //setInvites(invites.filter(item => item.id !== id));
-        //handleData();
+      if (res.status === 200) {
+        datafetching();
       }
+    } catch (err) {
+      console.log("request couldn't be cancelled", err);
     }
-    catch (err) {
-      console.log("request couldn't be cancelled", err)
-    }
-  }
+  };
 
   let [fontsLoaded] = useFonts({
     Poppins_600SemiBold,
-    Poppins_400Regular
+    Poppins_400Regular,
   });
 
   if (!fontsLoaded) {
@@ -113,78 +141,79 @@ const PageFriends = () => {
       <ScrollView
         contentContainerStyle={styles.screen}
         refreshControl={
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={datafetching}
-          />
+          <RefreshControl refreshing={refreshing} onRefresh={datafetching} />
         }
       >
         <Bg style={styles.wave} />
         <View>
           <Text style={styles.title}>Friends</Text>
-          {
-            friends.length ?
-              friends.map((item, index) => (
-                <View style={styles.card} key={index}>
+          {friends.length ? (
+            friends.map((item, index) => (
+              <View style={styles.card} key={index}>
                 <View style={styles.wrapperTop}>
                   <View style={styles.joined}>
-                      <ProfilePic />
+                    <ProfilePic />
                     <Text style={styles.title}>{item.name}</Text>
                   </View>
 
-                  <SecondaryBtn text={"REMOVE"} onPress={() => handleRemoveFriend(item.id)}></SecondaryBtn>
+                  <SecondaryBtn
+                    text={"REMOVE"}
+                    onPress={() => deleteFriend(item.id)}
+                  ></SecondaryBtn>
                 </View>
               </View>
-              ))
-              :
-              <Text>No friends yet! Make some friends by sending a friend request!</Text>}
+            ))
+          ) : (
+            <Text>
+              No friends yet! Make some friends by sending a friend request!
+            </Text>
+          )}
         </View>
 
         <View>
           <Text style={styles.title}>Invited</Text>
-          {invites.length
-            ?
+          {invites.length ? (
             invites.map((item, index) => (
-
               <View style={styles.card} key={index}>
                 <View style={styles.wrapperTop}>
                   <View style={styles.joined}>
-                      <ProfilePic />
+                    <ProfilePic />
                     <Text style={styles.title}>{item.name}</Text>
                   </View>
 
-                  <SecondaryBtn text={"Cancel"} onPress={() => handleCancelRequest(item.id)}></SecondaryBtn>
+                  <SecondaryBtn
+                    text={"Cancel"}
+                    onPress={() => cancelInvite(item.id)}
+                  ></SecondaryBtn>
                 </View>
               </View>
             ))
-            : (
-              <Text>No invitations sended</Text>
-            )}
-            </View>
+          ) : (
+            <Text>No invitations sended</Text>
+          )}
+        </View>
         <View>
           <Text style={styles.title}>Other people</Text>
-          {otherPeople.length
-            ?
+          {otherPeople.length ? (
             otherPeople.map((item, index) => (
-
               <View style={styles.card} key={index}>
                 <View style={styles.wrapperTop}>
                   <View style={styles.joined}>
-                     <ProfilePic />    
+                    <ProfilePic />
                     <Text style={styles.title}>{item.name}</Text>
                   </View>
 
-                  <SecondaryBtn text={"INVITE"} onPress={() => handleAddFriends(item.id)}></SecondaryBtn>
+                  <SecondaryBtn
+                    text={"INVITE"}
+                    onPress={() => sendInvite(item.id)}
+                  ></SecondaryBtn>
                 </View>
               </View>
             ))
-            : (
-              <Text>No users</Text>
-            )}
+          ) : (
+            <Text>No users</Text>
+          )}
         </View>
-
-
-
       </ScrollView>
     </SafeAreaView>
   );
@@ -201,18 +230,15 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     alignItems: "center",
-
   },
   surface: {
     borderRadius: 5,
     paddingRight: 10,
     marginHorizontal: 10,
     marginVertical: 6,
-    fontFamily: 'Poppins_600SemiBold'
+    fontFamily: "Poppins_600SemiBold",
   },
-  touchcard: {
-
-  },
+  touchcard: {},
   pfp: {
     height: 45,
     width: 45,
